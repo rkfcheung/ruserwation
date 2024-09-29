d52d407e654f629ab3115f4de987cf3804944956
Raymond Cheung <korekontakt@gmail.com>
2024-09-29 11:02:13 +0100

    Create app mod

--- src/app/models.rs
+++ src/app/models.rs
@@ -1,36 +1,31 @@
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-pub enum ReservationStatus {
-    Pending,
-    Confirmed,
-    Cancelled,
-}
+use crate::app::enums::ReservationStatus;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Customer {
-    pub id: i32,
+    pub id: u32,
     pub email: String,
     pub name: String,
     pub phone: String,
     pub last_reservation_time: NaiveDateTime,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Reservation {
-    pub id: i32,
-    pub restaurant_id: i32,
-    pub customer_id: i32,
+    pub id: u32,
+    pub restaurant_id: u32,
+    pub customer_id: u32,
     pub table_size: u8,
     pub reservation_time: NaiveDateTime,
     pub notes: Option<String>,
     pub status: ReservationStatus,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Restaurant {
-    pub id: i32,
+    pub id: u32,
     pub name: String,
     pub max_capacity: u32,
     pub location: String,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod app;
+
+use crate::app::models::Restaurant;
+
 fn main() {
-    println!("Hello, world!");
+    let restaurant = Restaurant {
+        id: 1,
+        name: "Hello World".to_string(),
+        max_capacity: 64,
+        location: "0x0".to_string(),
+        active: true,
+    };
+    println!("{:?}", restaurant);
 }
