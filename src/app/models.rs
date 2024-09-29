f541607f63f6cb9f64abdac8f617a5124c1e4850
Raymond Cheung <korekontakt@gmail.com>
2024-09-29 12:00:39 +0100

    Add Admin

--- src/app/models.rs
+++ src/app/models.rs
@@ -1,7 +1,21 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2,
+};
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
 use crate::app::enums::ReservationStatus;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Admin {
+    pub id: u32,
+    pub username: String,
+    pub password: Vec<u8>,
+    pub email: String,
+    pub root: bool,
+    pub last_login_time: Option<NaiveDateTime>,
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Customer {
@@ -9,7 +23,7 @@ pub struct Customer {
     pub email: String,
     pub name: String,
     pub phone: String,
-    pub last_reservation_time: NaiveDateTime,
+    pub last_reservation_id: u32,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -30,4 +44,24 @@ pub struct Restaurant {
     pub max_capacity: u32,
     pub location: String,
     pub active: bool,
+}
+
+impl Admin {
+    pub fn new(id: u32, username: String, password: String, email: String) -> Self {
+        let argon2 = Argon2::default();
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = argon2
+            .hash_password(password.as_bytes(), &salt)
+            .expect("Failed to hash password")
+            .to_string();
+
+        Admin {
+            id,
+            username,
+            password: password_hash.into_bytes(),
+            email,
+            root: id == 1,
+            last_login_time: None,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 mod app;
 
-use crate::app::models::Restaurant;
+use argon2::{
+    password_hash::{PasswordHash, PasswordVerifier},
+    Argon2,
+};
+use rand::{distributions::Alphanumeric, Rng};
+use std::str::from_utf8;
+
+use crate::app::models::{Admin, Restaurant};
 
 fn main() {
+    let rand_pwd: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(16)
+        .map(char::from)
+        .collect();
+    let admin = Admin::new(
+        1,
+        "admin".to_string(),
+        rand_pwd.clone(),
+        "admin@localhost".to_string(),
+    );
+    println!("Admin Random Password: {}", rand_pwd);
+    println!("{:?}", admin);
+    let parsed_hash = PasswordHash::new(from_utf8(&admin.password).unwrap()).unwrap();
+    assert!(Argon2::default()
+        .verify_password(rand_pwd.as_bytes(), &parsed_hash)
+        .is_ok());
+
     let restaurant = Restaurant {
         id: 1,
         name: "Hello World".to_string(),
