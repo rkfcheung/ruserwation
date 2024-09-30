1d8802ab3c73420fc7c4dcf4912255d2d2b637ba
Raymond Cheung <korekontakt@gmail.com>
2024-10-01 00:12:08 +0100

    Extract Sessions

--- src/admin/repo.rs
+++ src/admin/repo.rs
@@ -1,4 +1,3 @@
-use chrono::Utc;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use warp_sessions::Session;
@@ -6,67 +5,84 @@ use warp_sessions::Session;
 use super::models::Admin;
 
 pub trait AdminRepo {
-    fn init() -> Self; // Initialise the repository
+    fn new() -> Self; // Initialise the repository
     fn find_by_username(&self, username: &str) -> Option<Admin>; // Find an Admin by username
     fn save(&mut self, admin: Admin) -> u32; // Save an Admin and return its ID
     fn verify(&self, username: &str, password: &str) -> bool; // Verify username and password
 }
 
+pub struct Sessions {
+    context: Arc<Mutex<HashMap<String, Session>>>, // Session ID to Session mapping
+}
+
+impl Sessions {
+    pub fn new() -> Self {
+        Sessions {
+            context: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn create(&self, username: &str) -> String {
+        // Create a new session
+        let mut session = Session::new();
+        session.insert_raw("user", username.to_string());
+
+        let session_id = session.id().to_owned();
+
+        // Lock the sessions HashMap and insert the new session
+        let mut sessions = self.context.lock().unwrap();
+        sessions.insert(session_id.clone(), session);
+
+        // Return the session ID
+        session_id
+    }
+
+    pub fn destroy(&self, session_id: &str) {
+        let mut sessions = self.context.lock().unwrap();
+        sessions.remove(session_id);
+    }
+
+    pub fn get(&self, session_id: &str) -> Option<Session> {
+        let sessions = self.context.lock().unwrap();
+        sessions.get(session_id).cloned()
+    }
+}
+
 pub struct InMemoryAdminRepo {
     admins: Arc<Mutex<HashMap<String, Admin>>>, // Using username as the key
-    sessions: Arc<Mutex<HashMap<String, Session>>>, // Session ID to Session mapping
+    sessions: Sessions,                         // Session ID to Session mapping
 }
 
 impl InMemoryAdminRepo {
     pub fn new() -> Self {
         InMemoryAdminRepo {
             admins: Arc::new(Mutex::new(HashMap::new())),
-            sessions: Arc::new(Mutex::new(HashMap::new())),
+            sessions: Sessions::new(),
         }
     }
 
-    pub fn create_session(&mut self, username: &str) -> String {
-        let mut admins = self.admins.lock().unwrap();
+    pub fn create_session(&self, username: &str) -> String {
+        let admins = self.admins.lock().unwrap();
 
-        if let Some(mut admin) = admins.get_mut(username) {
-            let mut session = Session::new();
-            session.insert_raw("user", username.to_string());
-            let session_id = session.id().to_owned();
-
-            admin.last_login_time = Some(Utc::now().naive_utc());
-            self.save(admin.clone());
-
-            let mut sessions = self.sessions.lock().unwrap();
-            sessions.insert(session_id.clone(), session);
-            session_id
+        if admins.contains_key(username) {
+            self.sessions.create(username)
         } else {
             panic!("Username not found");
         }
     }
 
-    pub fn get_session(&self, session_id: &str) -> Option<Session> {
-        let sessions = self.sessions.lock().unwrap();
-        sessions.get(session_id).cloned()
+    pub fn destroy_session(&self, session_id: &str) {
+        self.sessions.destroy(session_id);
     }
 
-    pub fn destroy_session(&self, session_id: &str) {
-        let mut sessions = self.sessions.lock().unwrap();
-        sessions.remove(session_id);
+    pub fn get_session(&self, session_id: &str) -> Option<Session> {
+        self.sessions.get(session_id)
     }
 }
 
 impl AdminRepo for InMemoryAdminRepo {
-    fn init() -> Self {
+    fn new() -> Self {
         Self::new()
-    }
-
-    fn verify(&self, username: &str, password: &str) -> bool {
-        let admins = self.admins.lock().unwrap();
-        if let Some(admin) = admins.get(username) {
-            admin.verify_password(password)
-        } else {
-            false
-        }
     }
 
     fn find_by_username(&self, username: &str) -> Option<Admin> {
@@ -78,5 +94,14 @@ impl AdminRepo for InMemoryAdminRepo {
         let mut admins = self.admins.lock().unwrap();
         admins.insert(admin.username.clone(), admin.clone());
         admin.id
+    }
+
+    fn verify(&self, username: &str, password: &str) -> bool {
+        let admins = self.admins.lock().unwrap();
+        if let Some(admin) = admins.get(username) {
+            admin.verify_password(password)
+        } else {
+            false
+        }
     }
 }
--- tests/admin_repo.rs
+++ tests/admin_repo.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use chrono::Utc;
     use ruserwation::admin::{
         models::Admin,
         repo::{AdminRepo, InMemoryAdminRepo},
@@ -8,7 +9,7 @@ mod tests {
     #[test]
     fn test_in_memory_admin_repo() {
         // Create an instance of InMemoryAdminRepo
-        let mut repo = InMemoryAdminRepo::init();
+        let mut repo = InMemoryAdminRepo::new();
 
         // Create an Admin
         let admin = Admin::new(
@@ -25,7 +26,7 @@ mod tests {
         assert_eq!(saved_id, admin.id);
 
         // Retrieve the Admin by username
-        let found_admin = repo.find_by_username("admin").unwrap();
+        let mut found_admin = repo.find_by_username("admin").unwrap();
         assert_eq!(found_admin.username, admin.username);
         assert_eq!(found_admin.email, admin.email);
         assert!(found_admin.root);
@@ -36,6 +37,16 @@ mod tests {
 
         // Create a session for the admin
         let session_id = repo.create_session("admin");
+
+        // Update the last login time for the admin
+        found_admin.last_login_time = Some(Utc::now().naive_utc());
+
+        // Save the updated admin back to the repository
+        repo.save(found_admin.clone());
+        assert_eq!(
+            repo.find_by_username("admin").unwrap().last_login_time,
+            found_admin.last_login_time
+        );
 
         // Retrieve the session
         let session = repo.get_session(&session_id).unwrap();
