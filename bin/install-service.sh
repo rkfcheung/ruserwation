4d1e5e04672b7503eb52b04ae60b1d1d2151fd5f
Raymond Cheung <korekontakt@gmail.com>
2024-10-17 20:49:35 +0100

    Fix instll script

--- bin/install-service.sh
+++ bin/install-service.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+sudo cp ${THIS_DIR}/../etc/services/ruserwation.service /etc/systemd/system/ruserwation.service
+
 sudo systemctl daemon-reload
 sudo systemctl enable ruserwation
 sudo systemctl start ruserwation
