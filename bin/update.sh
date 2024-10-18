cb7121101c790affb075e146e9b7bd13d60c6491
Raymond Cheung <korekontakt@gmail.com>
2024-10-18 21:26:43 +0100

    Check errors on update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-THIS_DIR=$(cd $(dirname $0); pwd)
-
 echo "Updating latest codes ..."
 git pull
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to update codes"
+    exit 1
+fi
 
 echo "Building ..."
 cargo build --release
+if [ $? -ne 0 ]; then
+    echo "ERROR: Build failed"
+    exit 1
+fi
 
 echo "Restarting the service ..."
 sudo systemctl restart ruserwation
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to restart the service"
+    exit 1
+fi
+
+echo "Deployment successful!"
 sudo systemctl status ruserwation
