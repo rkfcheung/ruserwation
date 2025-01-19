36a42e600675e514d9c14cd34e49cd83152c97d2
Raymond Cheung <korekontakt@gmail.com>
2025-01-19 13:13:58 +0000

    Add reserve

--- src/reservation/reserve.rs
+++ src/reservation/reserve.rs
@@ -9,11 +9,14 @@ use warp::{
 };
 
 use super::{
-    helper::validate_reservation,
+    helper::{validate_ref_check, validate_reservation},
     models::{Reservation, ReservationRequest, ReservationResponse},
     repo::ReservationRepo,
 };
-use crate::config::{context::with_context, models::Context};
+use crate::{
+    config::{context::with_context, models::Context},
+    utils::env_util::{var_as_int_or, var_as_str_or},
+};
 
 pub fn reserve(
     context: Arc<Context<impl ReservationRepo + Send + Sync>>,
@@ -30,29 +33,52 @@ async fn handle_reserve(
     body: ReservationRequest,
     context: Arc<Context<impl ReservationRepo + Send + Sync>>,
 ) -> Result<impl Reply, Rejection> {
+    let secret = var_as_str_or("RW_RSV_SECRET", "ChangeMe");
+    let expired = var_as_int_or("RW_RSV_EXPIRED", 3_600) as i64;
+
+    // Validate ref_check
+    if let Err(err) = validate_ref_check(body.ref_check(), &secret, expired) {
+        log::error!(
+            "Failed to process reservation ref_check={} due to: {}",
+            body.ref_check(),
+            err
+        );
+
+        return Err(warp::reject::custom(ReservationResponse::err(
+            "Invalid Request or expired",
+        )));
+    }
+
+    // Convert to Reservation
     let mut reservation: Reservation = body.into();
-    let validated = validate_reservation(&reservation);
-    match validated {
-        Ok(_) => {
-            let saved = context.get().save(&mut reservation).await;
-            match saved {
-                Ok(id) => {
-                    log::info!(
-                        "Reservation id={id} with book_ref={} saved",
-                        reservation.book_ref
-                    );
-                    let response = json(&to_json!(ReservationResponse::ok(&reservation.book_ref)));
-                    Ok(with_status(response, StatusCode::OK))
-                }
-                Err(err) => {
-                    let response = json(&to_json!(ReservationResponse::err(&err.to_string())));
-                    Ok(with_status(response, StatusCode::INTERNAL_SERVER_ERROR))
-                }
-            }
+
+    // Validate reservation details
+    if let Err(err) = validate_reservation(&reservation) {
+        return Err(warp::reject::custom(ReservationResponse::err(&err)));
+    }
+
+    // Save reservation to the repository
+    match context.get().save(&mut reservation).await {
+        Ok(id) => {
+            log::info!(
+                "Reservation id={id} with book_ref={} saved",
+                reservation.book_ref
+            );
+
+            // Construct success response
+            let response = json(&to_json!(ReservationResponse::ok(&reservation.book_ref)));
+            Ok(with_status(response, StatusCode::OK))
         }
         Err(err) => {
-            let response = json(&to_json!(ReservationResponse::err(&err)));
-            Ok(with_status(response, StatusCode::BAD_REQUEST))
+            log::error!(
+                "Failed to save reservation book_ref={} due to: {}",
+                reservation.book_ref,
+                err
+            );
+
+            // Construct error response
+            let response = json(&to_json!(ReservationResponse::err(&err.to_string())));
+            Ok(with_status(response, StatusCode::INTERNAL_SERVER_ERROR))
         }
     }
 }
